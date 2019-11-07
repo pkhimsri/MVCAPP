86edda67149f2f4a6a7669797b7709735c7e1e41
pkhimsri <himanshu.srivastava@mind-infotech.com>
2019-11-07 14:21:42 +0530

    Add files via upload

---
+++ Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="TestAppOverDevOpsServer.MvcApplication" Language="C#" %>
---
+++ Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(TestAppOverDevOpsServer.Startup))]
+namespace TestAppOverDevOpsServer
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
